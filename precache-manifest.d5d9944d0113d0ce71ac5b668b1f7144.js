1451b097ff9cbce720c4196dd3fe840f97af3c4e
Anitra <anitra.oursky@gmail.com>
2019-09-03 14:46:03 -0400

    Updates

---
+++ precache-manifest.d5d9944d0113d0ce71ac5b668b1f7144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791176c1ca9061f83f060ea534f5006d",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "010d0dae5e7c7c03b4b4",
+    "url": "/react-clicky-game/static/css/main.df62a0b4.chunk.css"
+  },
+  {
+    "revision": "b026907f0b7d183e1c1c",
+    "url": "/react-clicky-game/static/js/2.ec4acc72.chunk.js"
+  },
+  {
+    "revision": "010d0dae5e7c7c03b4b4",
+    "url": "/react-clicky-game/static/js/main.387b9ec9.chunk.js"
+  },
+  {
+    "revision": "d8993df53f7ea0b767c1",
+    "url": "/react-clicky-game/static/js/runtime~main.92700e97.js"
+  }
+]);
